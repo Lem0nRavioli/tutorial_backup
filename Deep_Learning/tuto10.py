415861b70dd99d59019dcd7ae28fdb3d6d3bfb28
Lem0nRavioli <hirona_mj@hotmail.fr>
2021-02-01 15:11:28 +0100

    01/02/21

---
+++ Deep_Learning/tuto10.py
@@ -0,0 +1,91 @@
+""" RNN """
+import silence_tensorflow.auto
+from tensorflow.keras.layers import Embedding, Flatten, Dense, RNN, SimpleRNN, LSTM
+from tensorflow.keras.models import Sequential
+from tensorflow.keras import preprocessing
+from tensorflow.keras.datasets import imdb
+import os
+import numpy as np
+from util_func import plot_acc_loss
+
+
+def rnn_toy_example():
+    timesteps = 100
+    input_features = 32
+    output_features = 64
+
+    inputs = np.random.random((timesteps, input_features))
+    state_t = np.zeros((output_features,))
+
+    W = np.random.random((output_features, input_features))
+    U = np.random.random((output_features, output_features))
+    b = np.random.random((output_features,))
+
+    successive_outputs = []
+    for input_t in inputs:
+        output_t = np.tanh(np.dot(W, input_t) + np.dot(U, state_t) + b)
+        successive_outputs.append(output_t)
+        state_t = output_t
+
+    final_output_sequence = np.concatenate(successive_outputs, axis=0)
+    return final_output_sequence
+
+
+def sample_architecture():
+    """ return_sequence=True return every output at timestep t
+        Necessary for stacking multiple RNN that will require a time factor to compute """
+    model = Sequential()
+    model.add(Embedding(10000, 32))
+    model.add(SimpleRNN(32))
+    model.summary()
+
+    model = Sequential()
+    model.add(Embedding(10000, 32))
+    model.add(SimpleRNN(32, return_sequences=True))
+    model.add(SimpleRNN(32, return_sequences=True))
+    model.add(SimpleRNN(32, return_sequences=True))
+    model.add(SimpleRNN(32))
+    model.summary()
+
+
+max_features = 10000
+maxlen = 500
+epochs = 10
+
+print('loading data...')
+(input_train, y_train), (input_test, y_test) = imdb.load_data(num_words=max_features)
+print(len(input_train), 'train sequences')
+print(len(input_test), 'test sequences')
+
+print('Pad sequences (samples x time)')
+input_train = preprocessing.sequence.pad_sequences(input_train, maxlen=maxlen)
+input_test = preprocessing.sequence.pad_sequences(input_test, maxlen=maxlen)
+print('input_train shape:', input_train.shape)
+print('input_test shape:', input_test.shape)
+
+
+def train_plot_simple_RNN():
+    model = Sequential()
+    model.add(Embedding(max_features, 32))
+    model.add(SimpleRNN(32))
+    model.add(Dense(1, activation='sigmoid'))
+    model.compile(optimizer='rmsprop', loss='binary_crossentropy', metrics=['acc'])
+    history = model.fit(input_train, y_train, epochs=10, batch_size=128, validation_split=.2)
+
+    plot_acc_loss(history, epochs)
+
+
+def train_plot_LSTM():
+    model = Sequential()
+    model.add(Embedding(max_features, 32))
+    model.add(LSTM(32))
+    model.add(Dense(1, activation='sigmoid'))
+    model.compile(optimizer='rmsprop', loss='binary_crossentropy', metrics=['acc'])
+    history = model.fit(input_train, y_train, epochs=10, batch_size=128, validation_split=.2)
+
+    plot_acc_loss(history, epochs)
+
+
+train_plot_LSTM()
+
+# p230
--- Deep_Learning/tuto9.py
+++ Deep_Learning/tuto9.py
@@ -5,33 +5,35 @@ from tensorflow.keras.layers import Embedding, Flatten, Dense
 from tensorflow.keras.datasets import imdb
 from tensorflow.keras.models import Sequential
 from tensorflow.keras import preprocessing
-
-""" Toy example """
-'''# embedding_layer = Embedding(1000, 64)
-max_feature = 10000
-maxlen = 20
-
-(x_train, y_train), (x_test, y_test) = imdb.load_data(num_words=max_feature)
-x_train = preprocessing.sequence.pad_sequences(x_train, maxlen=maxlen)
-x_test = preprocessing.sequence.pad_sequences(x_test, maxlen=maxlen)
-
-model = Sequential()
-# 10000 => max vocab size, 8 => Embedding vector length, input_length => len(sample)
-# output will be (len(sampleS), len(sample), 8)
-# https://keras.io/api/layers/core_layers/embedding/
-model.add(Embedding(10000, 8, input_length=maxlen))
-model.add(Flatten())
-model.add(Dense(1, activation='sigmoid'))
-model.compile(optimizer='rmsprop', loss='binary_crossentropy', metrics=['acc'])
-model.summary()
-
-history = model.fit(x_train, y_train, epochs=10, batch_size=32, validation_split=.2)'''
-
-""" Using pretrained word embedding """
-
 import os
 import numpy as np
 from util_func import plot_acc_loss
+
+def train_toy_example():
+    # embedding_layer = Embedding(1000, 64)
+    max_feature = 10000
+    maxlen = 20
+
+    (x_train, y_train), (x_test, y_test) = imdb.load_data(num_words=max_feature)
+    x_train = preprocessing.sequence.pad_sequences(x_train, maxlen=maxlen)
+    x_test = preprocessing.sequence.pad_sequences(x_test, maxlen=maxlen)
+
+    model = Sequential()
+    # 10000 => max vocab size, 8 => Embedding vector length, input_length => len(sample)
+    # output will be (len(sampleS), len(sample), 8)
+    # https://keras.io/api/layers/core_layers/embedding/
+    model.add(Embedding(10000, 8, input_length=maxlen))
+    model.add(Flatten())
+    model.add(Dense(1, activation='sigmoid'))
+    model.compile(optimizer='rmsprop', loss='binary_crossentropy', metrics=['acc'])
+    model.summary()
+
+    history = model.fit(x_train, y_train, epochs=10, batch_size=32, validation_split=.2)
+    plot_acc_loss(history, 10)
+
+
+""" Using pretrained word embedding """
+
 
 imdb_dir = '../DataSets/aclimdb'
 train_dir = os.path.join(imdb_dir, 'train')
@@ -52,7 +54,7 @@ for label_type in ['neg', 'pos']:
                 labels.append(1)
 
 maxlen = 100
-training_samples = 200
+training_samples = 2000
 validation_samples = 10000
 max_words = 10000
 embedding_dim = 100
@@ -101,20 +103,70 @@ for word, i in word_index.items():
             embedding_matrix[i] = embedding_vector
 
 
-model = Sequential()
-model.add(Embedding(max_words, embedding_dim, input_length=maxlen))
-model.add(Flatten())
-model.add(Dense(32, activation='relu'))
-model.add(Dense(1, activation='sigmoid'))
-model.summary()
+def make_model():
+    model = Sequential()
+    model.add(Embedding(max_words, embedding_dim, input_length=maxlen))
+    model.add(Flatten())
+    model.add(Dense(32, activation='relu'))
+    model.add(Dense(1, activation='sigmoid'))
+    model.compile(optimizer='rmsprop', loss='binary_crossentropy', metrics=['acc'])
+    # model.summary()
+    return model
 
-model.layers[0].set_weights([embedding_matrix])
-model.layers[0].trainable = False
 
-model.compile(optimizer='rmsprop', loss='binary_crossentropy', metrics=['acc'])
-history = model.fit(x_train, y_train, epochs=epochs, batch_size=32, validation_data=(x_val, y_val))
-model.save_weights('pre_trained_glove_model.h5')
+def compile_fit(model):
+    history = model.fit(x_train, y_train, epochs=epochs, batch_size=32, validation_data=(x_val, y_val))
+    return model, history
 
-plot_acc_loss(history, epochs)
 
-# p216
+def train_model_pretrained_embedding(model, save=False):
+    model.layers[0].set_weights([embedding_matrix])
+    model.layers[0].trainable = False
+
+    model, history = compile_fit(model)
+    if save:
+        model.save_weights('pre_trained_glove_model.h5')
+    plot_acc_loss(history, epochs)
+
+
+def train_model_fresh_embedding(model, save=False):
+    model, history = compile_fit(model)
+    if save:
+        model.save_weights('fresh_embedding_model.h5')
+    plot_acc_loss(history, epochs)
+
+
+def compare_fresh_pretrained():
+    model_frozen = make_model()
+    model_fresh = make_model()
+    train_model_pretrained_embedding(model_frozen)
+    train_model_fresh_embedding(model_fresh)
+    # pretrained perform a bit better with fewer samples, but outperformed quickly with more
+    # 200 samples => frozen 55% acc vs 50% acc for fresh, 2000 samples, frozen 65% vs fresh 75%
+
+
+imdb_dir = '../DataSets/aclimdb'
+test_dir = os.path.join(imdb_dir, 'test')
+
+labels = []
+texts = []
+
+for label_type in ['neg', 'pos']:
+    dir_name = os.path.join(train_dir, label_type)
+    for fname in os.listdir(dir_name):
+        if fname[-4:] == '.txt':
+            f = open(os.path.join(dir_name, fname), encoding='utf8')
+            texts.append(f.read())
+            f.close()
+            if label_type == 'neg':
+                labels.append(0)
+            else:
+                labels.append(1)
+
+
+sequences = tokenizer.texts_to_sequences(texts)
+x_test = preprocessing.sequence.pad_sequences(sequences, maxlen=maxlen)
+y_test = np.asarray(labels)
+model = make_model()
+model.load_weights('pre_trained_glove_model.h5')
+model.evaluate(x_test, y_test)
