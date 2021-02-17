957dea66b9bc07a6466664e20e5e12cc39817243
Lem0nRavioli <hirona_mj@hotmail.fr>
2021-02-17 01:53:06 +0100

    17/02/21

--- Deep_Learning/tuto18.py
+++ Deep_Learning/tuto18.py
@@ -1,3 +1,86 @@
-""" Test generation with LSTM """
+""" Test generation with LSTM p 296 """
 
-# p296
+import numpy as np
+import silence_tensorflow.auto
+from tensorflow import keras
+import random
+import sys
+
+
+# Resampling softmax with softmax temperature
+# higher temp = more random
+def reweight_distribution(original_distribution, temperature=.5):
+    distribution = np.log(original_distribution) / temperature
+    distribution = np.exp(distribution)
+    return distribution / np.sum(distribution)
+
+
+path = keras.utils.get_file('nietzsche.txt', origin='https://s3.amazonaws.com/text-datasets/nietzsche.txt')
+text = open(path).read().lower()
+print('Corpus length:', len(text))
+maxlen = 60
+step = 3
+sentences = []
+next_chars = []
+
+for i in range(0, len(text) - maxlen, step):
+    sentences.append(text[i: i + maxlen])
+    next_chars.append(text[i + maxlen])
+
+print('Number of sequences:', len(sentences))
+
+chars = sorted(list(set(text)))  # list of unique characters in the corpus
+print('Unique characters:', len(chars))
+char_indices = dict((char, chars.index(char)) for char in chars)
+
+print('Vectorization into one hot matrices...')
+x = np.zeros((len(sentences), maxlen, len(chars)), dtype=np.bool)
+y = np.zeros((len(sentences), len(chars)), dtype=np.bool)
+for i, sentence in enumerate(sentences):
+    for t, char in enumerate(sentence):
+        x[i, t, char_indices[char]] = 1
+    y[i, char_indices[next_chars[i]]] = 1
+
+model = keras.models.Sequential()
+model.add(keras.layers.LSTM(128, input_shape=(maxlen, len(chars))))
+model.add(keras.layers.Dense(len(chars), activation='softmax'))
+
+optimizer = keras.optimizers.RMSprop(lr=.01)
+model.compile(loss='categorical_crossentropy', optimizer=optimizer)
+
+
+""" np.random.multinomial(20, [1/6.]*6, size=1) 
+ >>>array([[4, 1, 7, 5, 2, 1]]) 
+ # proba of 20 throw of 6 face dice, landed 4 times on 1, 1 time on 2, 7 times on 3..."""
+
+
+def sample(preds, temperature=1.0):
+    preds = np.asarray(preds).astype('float64')
+    preds = np.log(preds) / temperature
+    exp_preds = np.exp(preds)
+    preds = exp_preds / np.sum(exp_preds)
+    probas = np.random.multinomial(1, preds, 1)
+    return np.argmax(probas)
+
+
+for epoch in range(1, 60):
+    print('epoch ', epoch)
+    model.fit(x, y, batch_size=128, epochs=1)
+    start_index = random.randint(0, len(text) - maxlen - 1)
+    generated_text = text[start_index: start_index + maxlen]
+    print('--- Generating with seed: "' + generated_text + '"')
+    for temperature in [.2, .5, 1., 1.2]:
+        print()
+        print('----- temperature:', temperature)
+        sys.stdout.write(generated_text)  # print something without going in line
+        for i in range(400):
+            sampled = np.zeros((1, maxlen, len(chars)))
+            for t, char in enumerate(generated_text):
+                sampled[0, t, char_indices[char]] = 1
+            preds = model.predict(sampled, verbose=0)[0]
+            next_index = sample(preds, temperature)
+            next_char = chars[next_index]
+            generated_text += next_char
+            generated_text = generated_text[1:]
+
+            sys.stdout.write(next_char)
