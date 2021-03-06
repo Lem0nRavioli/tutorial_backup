f321d3e2251c94eaf2289e007979d33d899fa29c
Lem0nRavioli <hirona_mj@hotmail.fr>
2021-03-06 13:51:57 +0100

    06/03/21

---
+++ Coursea/CnnTF/tuto2.py
@@ -0,0 +1,113 @@
+""" data augmentation for images"""
+
+import silence_tensorflow.auto
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.optimizers import RMSprop
+import os
+import matplotlib.pyplot as plt
+
+
+
+
+
+
+base_dir = '../../DataSets/Cat_Dog/subset/'
+train_dir = os.path.join(base_dir, 'train')
+validation_dir = os.path.join(base_dir, 'validation')
+
+# Directory with our training cat pictures
+train_cats_dir = os.path.join(train_dir, 'cat')
+
+# Directory with our training dog pictures
+train_dogs_dir = os.path.join(train_dir, 'dog')
+
+# Directory with our validation cat pictures
+validation_cats_dir = os.path.join(validation_dir, 'cat')
+
+# Directory with our validation dog pictures
+validation_dogs_dir = os.path.join(validation_dir, 'dog')
+
+model = tf.keras.models.Sequential([
+    tf.keras.layers.Conv2D(32, (3, 3), activation='relu', input_shape=(150, 150, 3)),
+    tf.keras.layers.MaxPooling2D(2, 2),
+    tf.keras.layers.Conv2D(64, (3, 3), activation='relu'),
+    tf.keras.layers.MaxPooling2D(2, 2),
+    tf.keras.layers.Conv2D(128, (3, 3), activation='relu'),
+    tf.keras.layers.MaxPooling2D(2, 2),
+    tf.keras.layers.Conv2D(128, (3, 3), activation='relu'),
+    tf.keras.layers.MaxPooling2D(2, 2),
+    tf.keras.layers.Dropout(.5),
+    tf.keras.layers.Flatten(),
+    tf.keras.layers.Dense(512, activation='relu'),
+    tf.keras.layers.Dense(1, activation='sigmoid')
+])
+
+model.compile(loss='binary_crossentropy',
+              optimizer=RMSprop(lr=1e-4),
+              metrics=['accuracy'])
+
+# All images will be rescaled by 1./255
+# train_datagen = ImageDataGenerator(rescale=1. / 255)
+# augmented train_datagen
+train_datagen = ImageDataGenerator(rescale=1./255, rotation_range=40, width_shift_range=.2, height_shift_range=.2,
+                                   shear_range=.2, zoom_range=.2, horizontal_flip=True, fill_mode='nearest')
+test_datagen = ImageDataGenerator(rescale=1. / 255)
+
+
+# Flow training images in batches of 20 using train_datagen generator
+train_generator = train_datagen.flow_from_directory(
+    train_dir,  # This is the source directory for training images
+    target_size=(150, 150),  # All images will be resized to 150x150
+    batch_size=20,
+    # Since we use binary_crossentropy loss, we need binary labels
+    class_mode='binary')
+
+# Flow validation images in batches of 20 using test_datagen generator
+validation_generator = test_datagen.flow_from_directory(
+    validation_dir,
+    target_size=(150, 150),
+    batch_size=20,
+    class_mode='binary')
+
+history = model.fit(
+    train_generator,
+    steps_per_epoch=100,  # 2000 images = batch_size * steps
+    epochs=150,
+    validation_data=validation_generator,
+    validation_steps=50,  # 1000 images = batch_size * steps
+)
+
+
+def show_graph(history):
+    acc = history.history['accuracy']
+    val_acc = history.history['val_accuracy']
+    loss = history.history['loss']
+    val_loss = history.history['val_loss']
+
+    epochs = range(len(acc))
+
+    plt.plot(epochs, acc, 'bo', label='Training accuracy')
+    plt.plot(epochs, val_acc, 'b', label='Validation accuracy')
+    plt.title('Training and validation accuracy')
+    plt.legend()
+
+    plt.figure()
+
+    plt.plot(epochs, loss, 'bo', label='Training Loss')
+    plt.plot(epochs, val_loss, 'b', label='Validation Loss')
+    plt.title('Training and validation loss')
+    plt.legend()
+
+    plt.show()
+
+
+show_graph(history)
+
+"""
+Without data augmentation, 30 epochs, capping around 74% after 20 epochs
+With data augmentation, 30 epochs, still increasing after 30 epochs, 76%
+with data augmentation, 50 epochs, dropout 0.5 after cnn, still increasing after 50 epochs, 80%
+150 epochs, plateau around 90 at 82%
+"""
