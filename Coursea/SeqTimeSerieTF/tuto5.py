89f1d81c4ebe8464a874a60aba9de3fba3840276
Lem0nRavioli <hirona_mj@hotmail.fr>
2021-03-26 13:50:29 +0100

    26/03/21

--- Coursea/SeqTimeSerieTF/tuto5.py
+++ Coursea/SeqTimeSerieTF/tuto5.py
@@ -5,7 +5,7 @@ import tensorflow as tf
 import csv
 import numpy as np
 import matplotlib.pyplot as plt
-from tuto_utils.util_func import windowed_dataset, plot_series, show_me_lr
+from tuto_utils.util_func import windowed_dataset, plot_series, show_me_lr, model_forecast
 
 path = '../../DataSets/sunspot/Sunspots.csv'
 
@@ -23,17 +23,65 @@ time = range(len(series))
 # plot_series(time, series)
 # plt.show()
 
-split_time = 2550
+split_time = 3000 # 2550
 time_train = time[:split_time]
 x_train = series[:split_time]
 time_valid = time[split_time:]
 x_valid = series[split_time:]
 
-window_size = 20
-batch_size = 32
+window_size = 30  # 20
+batch_size = 64
 shuffle_buffer_size = False
 
 dataset = windowed_dataset(x_train, window_size, batch_size, shuffle_buffer_size)
+
+
+def graph_evaluate(model, epoch=100, verbose=0, show_me_shape=False):
+    history = model.fit(dataset, epochs=epoch, verbose=verbose)
+    forecast = []
+    forecast = model_forecast(model, series, window_size)
+    if show_me_shape:
+        print(forecast.shape)
+        print(forecast[split_time - window_size: -1, -1, 0].shape)
+        print(forecast[0])
+        print("boop 0 0")
+        print(forecast[0, 0])
+        print("boop 0 0 0")
+        print(forecast[0, 0, 0])
+    # for time in range(len(series) - window_size):
+    #     forecast.append(model.predict(series[time: time + window_size][np.newaxis]))
+
+    forecast = forecast[split_time - window_size:-1, -1, 0]
+
+    print("mae:", tf.keras.metrics.mean_absolute_error(x_valid, forecast).numpy())
+
+    plt.figure(figsize=(10, 6))
+    plot_series(time_valid, x_valid)
+    plot_series(time_valid, forecast)
+    plt.show()
+
+    loss = history.history['loss']
+    loss = loss[10: ]
+    epochs = range(len(loss))
+    plt.plot(epochs, loss, 'b', label='Training Loss')
+    plt.show()
+
+
+def basic_evaluate(model, epoch=100):
+    model.fit(dataset, epochs=100)
+
+    model.evaluate(windowed_dataset(x_valid, window_size, batch_size, shuffle_buffer_size))
+
+
+def model_0():
+    model = tf.keras.models.Sequential([
+        tf.keras.layers.Lambda(lambda x: tf.expand_dims(x, axis=-1), input_shape=[None]),
+        tf.keras.layers.Dense(20, activation='relu'),
+        tf.keras.layers.Dense(10, activation='relu'),
+        tf.keras.layers.Dense(1)
+    ])
+
+    return model
 
 
 def model_1():
@@ -64,9 +112,24 @@ def model_2():
     return model
 
 
-model = model_2()
-optimizer = tf.keras.optimizers.SGD(lr=1e-5, momentum=.9)
-model.compile(loss=tf.keras.losses.Huber(), optimizer='adam', metrics=['mae'])
-model.fit(dataset, epochs=100)
+def model_3():
+    model = tf.keras.models.Sequential([
+        tf.keras.layers.Lambda(lambda x: tf.expand_dims(x, axis=-1), input_shape=[None]),
+        tf.keras.layers.Conv1D(filters=32, kernel_size=5, strides=1, padding='causal',
+                               activation='relu'),
+        tf.keras.layers.Bidirectional(tf.keras.layers.LSTM(32, return_sequences=True)),
+        tf.keras.layers.Bidirectional(tf.keras.layers.LSTM(32, return_sequences=True)),
+        tf.keras.layers.Dense(20, activation='relu'),
+        tf.keras.layers.Dense(10, activation='relu'),
+        tf.keras.layers.Dense(1),
+        tf.keras.layers.Lambda(lambda x: x * 400)
+    ])
 
-model.evaluate(windowed_dataset(x_valid, window_size, batch_size, shuffle_buffer_size))
+    return model
+
+
+model = model_3()
+optimizer = tf.keras.optimizers.SGD(lr=1e-7, momentum=.9)
+# model.compile(loss=tf.keras.losses.Huber(), optimizer='adam', metrics=['mae'])
+model.compile(loss='mse', optimizer=optimizer)
+graph_evaluate(model, 300, 1)
