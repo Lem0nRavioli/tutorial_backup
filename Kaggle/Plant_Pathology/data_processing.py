85c1a2a70d4a50e6f36de1975aa1f99573295307
Lem0nRavioli <hirona_mj@hotmail.fr>
2021-03-31 05:44:52 +0200

    31/03/21

--- Kaggle/Plant_Pathology/data_processing.py
+++ Kaggle/Plant_Pathology/data_processing.py
@@ -2,8 +2,12 @@ import silence_tensorflow.auto
 import tensorflow as tf
 import csv
 import numpy as np
+from PIL import Image
+import os
 
-train_path = 'train.csv'
+base_path = "D:\Progs - D\DataSets\Plant_Pathology"
+base_path_train = os.path.join(base_path, "train_images")
+csv_path = os.path.join(base_path, "train.csv")
 
 
 def extract_filenames(path, have_labels=True):
@@ -28,10 +32,41 @@ def extract_filenames(path, have_labels=True):
     return filenames
 
 
-filenames, labels = extract_filenames(train_path)
+filenames, labels = extract_filenames(csv_path)
 
 print(filenames[:10])
 print(labels[:10])
 print(np.unique(labels))
 
 # then iterate over filenames and make a tensor of x*y*channel of pictures, then use datagen.flow
+
+
+def checkimagesize(paths):
+    sizes={}
+    for p in paths:
+        img = Image.open(os.path.join(base_path_train,p))
+        if str(img.size) in sizes:
+            sizes[str(img.size)]+=1
+        else:
+            sizes[str(img.size)]=1
+    print(sizes)
+
+
+# checkimagesize(filenames)
+# {'(4000, 2672)': 16485, '(4000, 3000)': 665, '(2592, 1728)': 1027, '(4608, 3456)': 123, '(5184, 3456)': 193,
+# '(4032, 3024)': 132, '(3024, 4032)': 3, '(3024, 3024)': 3, '(4000, 2248)': 1}
+
+def get_np_images(paths):
+    data = []
+    for p in paths:
+        img = Image.open(os.path.join(base_path_train, p))
+        data.append(np.array(img))
+    return data
+
+
+data = np.array(get_np_images(filenames))
+
+# need to find a way to flow from directory or squish them during loading in data
+# currently too big for memory
+
+print(data.shape)
